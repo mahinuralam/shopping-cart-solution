d8d084c65cc11a3419adab6005f87765e3d209c8
Md. Mahinur Alam Ononno <64003114+mahinuralam@users.noreply.github.com>
2022-04-29 16:08:53 +0600

    4/29/2022

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
 
-function upadeCaseNumber (isIncreasing) {
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+function updateProductNumber (product, price, isIncreasing) {
+
+    // Update Quanity 
+    const productInput = document.getElementById(product + '-number');
+    let caseNumber = productInput.value;
     if(isIncreasing == true)
     {
-        caseNumber.value = parseInt(caseNumber) + 1;
+        caseNumber = parseInt(caseNumber) + 1;
     }
     else
     {
         if(parseInt(caseNumber)>0)
         {
-            caseNumber.value = parseInt(caseNumber) - 1;
+            caseNumber = parseInt(caseNumber) - 1;
         }
     }
-    caseInput.value = caseNumber;
+    productInput.value = caseNumber;
 
-    // update case total
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerHTML = caseNumber * 59;
+    // Update price
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerHTML = caseNumber * price;
+    calculateToatal();
 }
 
+function getInputvalue(product){
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateToatal(){
+    const phoneTotal = getInputvalue('phone') * 1219;
+    const caseTotal = getInputvalue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+
+    // update on the html
+    document.getElementById('sub-total').innerHTML = subTotal;
+    document.getElementById('tax-amount').innerHTML = tax;
+    document.getElementById('total-price').innerHTML = totalPrice;
+    
+
+}
+
+// Phone
+document.getElementById('phone-plus').addEventListener
+('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+
+document.getElementById('phone-minus').addEventListener
+('click', function () {
+    updateProductNumber('phone', 1219, false);
+});
+
+// Case
 document.getElementById('case-plus').addEventListener
 ('click', function () {
-    upadeCaseNumber(true);
+    updateProductNumber('case', 59, true);
 });
 
 document.getElementById('case-minus').addEventListener
 ('click', function () {
-    upadeCaseNumber(false);
+    updateProductNumber('case', 59, false);
 });
